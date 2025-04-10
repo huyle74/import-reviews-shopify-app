cfec71333414999dd0a492ce717e5590f96d9fc3
huyle74 <legiahuy74@gmail.com>
2025-04-10 17:54:33 +0700

    update headers in shops table

--- backend/src/handler/getHeader.googleExtension.js
+++ backend/src/handler/getHeader.googleExtension.js
@@ -2,12 +2,18 @@ const { query } = require("../utils/db");
 
 async function headerFromExtension(headers, shop_id) {
   try {
-    const result = await query("SELECT EXISTS(SELECT 1 FROM shops WHERE shop_id = $1)", [shop_id]);
-    const exists = result.rows[0].exists;
-    if (!exists) {
-      await query(`UPDATE shops SET headers = $1 WHERE shop_id = $2`, [headers, shop_id]);
-      console.log(`-----DONE ADD NEW HEADERS COOKIE: shop ${shop_id} -----`);
+    const addShop = await query(
+      `INSERT INTO shops (shop_id, headers)
+          VALUES ($1, $2)
+          ON CONFLICT (shop_id)
+          DO UPDATE SET headers = EXCLUDED.headers
+        RETURNING *;`,
+      [shop_id, headers]
+    );
+    if (addShop.rows.length > 0) {
+      console.log("Shop added successfully:", addShop.rows[0]);
     }
+    console.log(`-----DONE ADD NEW HEADERS COOKIE: shop ${shop_id} -----`);
   } catch (error) {
     console.error("GET HEADER FROM GOOGLE EXTENSION BUG HERE >>>> ", error);
     res.status(404).json({ ERROR: "cannot get header cookies" });
