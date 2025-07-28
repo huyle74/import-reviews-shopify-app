beaeffefc6973eebaa8a17d59a556145652429d2
letandat1607 <dat16072004@gmail.com>
2025-07-28 19:25:57 +0700

    delete unused files

--- frontend/app/components/import_reviews/importWithProduct.jsx
+++
@@ -1,5 +0,0 @@
-import { Box, Button, Card } from "@shopify/polaris";
-
-export default function ImportReview() {
-  return <Card>Hello World</Card>;
-}
--- frontend/app/components/manage_reviews/filterList/multiChoiceList.jsx
+++
@@ -1,5 +0,0 @@
-import { ChoiceList } from "@shopify/polaris";
-
-export default function MultiChoiceList({ title }) {
-  return <ChoiceList allowMultiple title={title} />;
-}
